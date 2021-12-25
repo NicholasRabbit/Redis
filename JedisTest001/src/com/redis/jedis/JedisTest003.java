0b6fc906164e1a03b7724ba41a5cf2f724854b67
plms <askdell@sina.com>
2021-12-25 16:14:53 +0800

    JedisTest003-list类型练习

---
+++ JedisTest001/src/com/redis/jedis/JedisTest003.java
@@ -0,0 +1,18 @@
+package com.redis.jedis;
+
+
+public class JedisTest003 {
+
+    public static void main(String[] args) {
+        redisList();
+    }
+
+    private static void redisList() {
+        /**
+         * Redis的List类型数据相关方法范例
+         * */
+
+
+
+    }
+}
